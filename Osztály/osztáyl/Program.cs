479ace5263a9a867768e2c27c6d77dad3eff31fb
szabomarcel <112942139+szabomarcel@users.noreply.github.com>
2023-02-20 09:53:34 +0100

    main

---
+++ Osztály/osztáyl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztály
+{
+    class Kutya
+    {
+        public Kutya(string nev) { }
+        public Kutya(string name) { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
